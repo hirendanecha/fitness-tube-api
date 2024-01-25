5028b93181df863cf18653a31f030099bd59a016
Swapnil Opash <sra.opash@gmail.com>
2024-01-25 18:41:50 +0530

    fixed

--- src/helpers/utils.js
+++ src/helpers/utils.js
@@ -106,7 +106,9 @@ exports.notificationMail = async (userData) => {
 
 exports.channelNotificationEmail = async (userData) => {
   let name = userData?.Username;
-  let msg = `you are assigned in HT channel by MasterAdmin`;
+  let msg = `You have been assign in FitnessTube channel by the FitnessTube Admin.
+  To access your channel, log into your FitnessTube account,click on the
+  FitnessTube icon at the top of the page,then click on My Channel.`;
   let redirectUrl = `${environment.FRONTEND_URL}`;
 
   const mailObj = {
@@ -131,9 +133,9 @@ exports.communityApproveEmail = async (profileId, isApprove) => {
       userData[0]?.FirstName + " " + userData[0]?.LastName;
     let msg = "";
     if (isApprove === "Y") {
-      msg = `Your Trainer application has been approved by Master Admin.`;
+      msg = `FitnessTube has approved your Trainer application.`;
     } else {
-      msg = `Your Trainer application has been unapproved by Master Admin.`;
+      msg = `FitnessTube has unapproved your Trainer application.`;
     }
     let redirectUrl = `${environment.FRONTEND_URL}`;
     const mailObj = {
